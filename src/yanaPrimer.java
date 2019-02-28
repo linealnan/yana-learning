68413f02504ff650ccfade353ace5700b1645422
Шкатов Дмитрий <shkatov.dmitry@gmail.com>
2019-02-28 14:59:12 +0500

    Проверяем примеры

---
+++ src/yanaPrimer.java
@@ -0,0 +1,39 @@
+/**
+ * Created by shkatova yana on 28.02.19.
+ */
+public class yanaPrimer {
+    public static void main(String args[]) {
+        float result1;
+        float result2;
+        float result3;
+        float result4;
+        float result5;
+        float result6;
+        float result7;
+        float result8;
+        float result9;
+        float result10;
+
+        result1 = 646 / 2;
+        result2 = 315 + 62;
+        result3 = 58 - 8 + 23;
+        result4 = 46 + 25;
+        result5 = 73 + 21 +2;
+        result6 = 33 +22 + 11;
+        result7 = 52 * 2;
+        result8 = 34 * 3;
+        result9 = 112 / 2;
+        result10 = 56 + 56;
+
+        System.out.println("Результат вычиcления первого примера равен: " + result1);
+        System.out.println("Результат вычиcления первого примера равен: " + result2);
+        System.out.println("Результат вычиcления первого примера равен: " + result3);
+        System.out.println("Результат вычиcления первого примера равен: " + result4);
+        System.out.println("Результат вычиcления первого примера равен: " + result5);
+        System.out.println("Результат вычиcления первого примера равен: " + result6);
+        System.out.println("Результат вычиcления первого примера равен: " + result7);
+        System.out.println("Результат вычиcления первого примера равен: " + result8);
+        System.out.println("Результат вычиcления первого примера равен: " + result9);
+        System.out.println("Результат вычиcления первого примера равен: " + result10);
+    }
+}
