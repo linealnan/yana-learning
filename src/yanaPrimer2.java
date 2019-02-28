a555deed4c981cb4f21ebf02a4bb592665c7e863
Шкатов Дмитрий <shkatov.dmitry@gmail.com>
2019-02-28 15:44:14 +0500

    Вторая проверка примеров

---
+++ src/yanaPrimer2.java
@@ -0,0 +1,30 @@
+/**
+ * Created by shkatovdm on 28.02.19.
+ */
+public class yanaPrimer2 {
+    public static void main(String args[]) {
+        int result1;
+        int result2;
+        int result3;
+        int result4;
+        int result5;
+        int result6;
+        int result7;
+
+        result1 = 426 + 112;
+        result2 = 642 / 2;
+        result3 = 346 + 112;
+        result4 = 206 + 34 - 2;
+        result5 = 56 + 22 + 31;
+        result6 = 41 + 26 + 7;
+        result7 = 34 + 34 + 5;
+
+        System.out.println("Результат вычиcления первого примера равен: " + result1);
+        System.out.println("Результат вычиcления первого примера равен: " + result2);
+        System.out.println("Результат вычиcления первого примера равен: " + result3);
+        System.out.println("Результат вычиcления первого примера равен: " + result4);
+        System.out.println("Результат вычиcления первого примера равен: " + result5);
+        System.out.println("Результат вычиcления первого примера равен: " + result6);
+        System.out.println("Результат вычиcления первого примера равен: " + result7);
+    }
+}
