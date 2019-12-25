9be1746970c4851803210dea44c49aca0dd1c5fc
Dmitriy Shkatov <Dmitriy.Shkatov@waveaccess.ru>
2019-12-25 22:57:36 +0500

    [NF] Вывод псевдографики

---
+++ src/digitOutput.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Shkatova Yana on 28.02.19.
+ */
+public class digitOutput {
+    public static void main(final String args[]) {
+        
+        String zero =   "********\n" +
+                        "*      *\n" +
+                        "*      *\n" +
+                        "*      *\n" +
+                        "*      *\n" +
+                        "********\n";
+
+        String one  =   "       *\n" +
+                        "      **\n" +
+                        "     * *\n" +
+                        "       *\n" +
+                        "       *\n" +
+                        "       *\n" +
+                        "       *\n";
+
+        String two  =   " *******\n" +
+                        " *     *\n" +
+                        " *    * \n" +
+                        "     *  \n" +
+                        "    *   \n" +
+                        "   *    \n" +
+                        " *******\n";
+
+        System.out.print("\n\n");
+        System.out.print(zero);
+        System.out.print("\n\n");
+        System.out.print(one);
+        System.out.print("\n\n");
+        System.out.print(two);
+        System.out.print("\n\n");
+    }
+}
